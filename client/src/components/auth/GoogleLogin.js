bb496a72d45d6ccb1e3bbfd8cd2dc67588796f86
Manoj-kandpal <manojkandpal02@gmail.com>
2023-04-23 08:54:50 +0530

    First commit

--- client/src/components/auth/GoogleLogin.js
+++ client/src/components/auth/GoogleLogin.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { Button } from "react-bootstrap";
 
 const GoogleLogin = () => {
-    const loginWithGoogle = async (ev) => {
-        ev.preventDefault();
-        window.open("http://localhost:5000/api/auth/google", "_self");
-        // const res = await api.get('/auth/google', config);
-    }
-    return (
-        <Button variant='danger' onClick={loginWithGoogle}><i className='fa-brands fa-google'/>&nbsp;&nbsp;Login</Button>
-    )
+  const loginWithGoogle = async (ev) => {
+    ev.preventDefault();
+    window.open(
+      "https://light-erin-leggings.cyclic.app/api/auth/google",
+      "_self"
+    );
+    // const res = await api.get('/auth/google', config);
+  };
+  return (
+    <Button variant="danger" onClick={loginWithGoogle}>
+      <i className="fa-brands fa-google" />
+      &nbsp;&nbsp;Login
+    </Button>
+  );
 };
 
 export default GoogleLogin;
